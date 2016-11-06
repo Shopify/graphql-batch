11bb30307453c546b2fdedac5374b1e78cdc1f37
Robert Mosolgo <rdmosolgo@gmail.com>
2016-11-06 08:54:54 -0500

    refactor(MutationExecutionStrategy) don't switch strategies during resolution

--- lib/graphql/batch/mutation_execution_strategy.rb
+++ lib/graphql/batch/mutation_execution_strategy.rb
@@ -4,15 +4,10 @@ module GraphQL::Batch
       def get_finished_value(raw_value)
         raw_value = GraphQL::Batch::Promise.resolve(raw_value).sync
 
-        context = execution_context.query.context
-        old_execution_strategy = context.execution_strategy
         begin
-          context.execution_strategy = GraphQL::Batch::ExecutionStrategy.new
           result = super(raw_value)
           GraphQL::Batch::Executor.current.wait_all
           result
-        ensure
-          context.execution_strategy = old_execution_strategy
         end
       end
     end
