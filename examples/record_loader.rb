0c2368319c47a7c5bf8ea95f19f426c364a5bb82
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2017-08-11 08:59:18 -0700

    Add Active Record AssociationLoader and RecordLoader examples

---
+++ examples/record_loader.rb
@@ -0,0 +1,28 @@
+class RecordLoader < GraphQL::Batch::Loader
+  def initialize(model, column: model.primary_key, where: nil)
+    @model = model
+    @column = column.to_s
+    @column_type = model.type_for_attribute(@column)
+    @where = where
+  end
+
+  def load(key)
+    super(@column_type.cast(key))
+  end
+
+  def perform(keys)
+    query(keys).each do |record|
+      value = @column_type.cast(record.public_send(@column))
+      fulfill(value, record)
+    end
+    keys.each { |key| fulfill(key, nil) unless fulfilled?(key) }
+  end
+
+  private
+
+  def query(keys)
+    scope = @model
+    scope = scope.where(@where) if @where
+    scope.where(@column => keys)
+  end
+end
