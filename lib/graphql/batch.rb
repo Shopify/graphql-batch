746045a5a418ac3d7fed3edc4756670c37245693
Marc-Andre Giroux <xuorig@github.com>
2017-12-13 18:43:52 -0500

    Allow a custom executor class in `GraphQL::Batch.batch` (#75)

--- lib/graphql/batch.rb
+++ lib/graphql/batch.rb
@@ -9,9 +9,9 @@ module GraphQL
     BrokenPromiseError = ::Promise::BrokenError
     class NoExecutorError < StandardError; end
 
-    def self.batch
+    def self.batch(executor_class: GraphQL::Batch::Executor)
       begin
-        GraphQL::Batch::Executor.start_batch GraphQL::Batch::Executor
+        GraphQL::Batch::Executor.start_batch(executor_class)
         ::Promise.sync(yield)
       ensure
         GraphQL::Batch::Executor.end_batch
--- test/custom_executor_test.rb
+++ test/custom_executor_test.rb
@@ -2,17 +2,29 @@ require_relative 'test_helper'
 
 class GraphQL::Batch::CustomExecutorTest < Minitest::Test
   class MyCustomExecutor < GraphQL::Batch::Executor
-    @@call_count = 0
-
-    def self.call_count
-      @@call_count
+    class << self
+      attr_accessor :call_count
     end
+    self.call_count = 0
 
     def around_promise_callbacks
-      @@call_count += 1
+      self.class.call_count += 1
 
       super
     end
+  end
+
+  def setup
+    MyCustomExecutor.call_count = 0
+  end
+
+  def test_batch_accepts_custom_executor
+    product = GraphQL::Batch.batch(executor_class: MyCustomExecutor) do
+      RecordLoader.for(Product).load(1)
+    end
+
+    assert_equal 'Shirt', product.title
+    assert MyCustomExecutor.call_count > 0
   end
 
   def test_custom_executor_class
